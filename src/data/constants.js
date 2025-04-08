6db14d54158afcae706415581608f6c375834f5a
sai-kumar-visai <v.s.kumar1824@gmail.com>
2025-04-09 00:28:24 +0530

    solution

---
+++ src/data/constants.js
@@ -0,0 +1,291 @@
+export const Bio = {
+  name: "Sai Kumar Visai",
+  roles: [
+    "Full Stack Developer",
+    "Android Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/sai-kumar-visai",
+  resume:
+    "https://drive.google.com/file/d/1YftnCUWB4yhkV9oJYynl_8nWrKDiB3Hy/view?usp=drivesdk",
+  linkedin: "https://www.linkedin.com/in/sai-kumar-visai/",
+  twitter: "https://twitter.com/RishavChanda",
+  insta: "https://www.instagram.com/v_s_kumar__/",
+  facebook: "https://www.facebook.com/rishav.chanda.165/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Spring Boot",
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/79/Spring_Boot.svg",
+      },
+    ],
+  },
+  {
+    title: "Databases",
+    skills: [
+      {
+        name: "MySQL",
+        image:
+          "https://www.svgrepo.com/show/303251/mysql-logo.svg",
+      },
+      {
+        name: "SQLite",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/SQLite370.svg/2560px-SQLite370.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFDKcWIhUtBGaaNIRal9FJi-BTV_4q5MowMw&s",
+    role: "Fullstack Developer",
+    company: "Nxt Wave",
+    date: "February 2024 - January 2025",
+    desc: "Pursuing an Industry Ready Certification in Java Full-Stack Development. With a solid academic foundation and practical experience in developing web applications using React and Spring Boot",
+    skills: [
+      "ReactJS",
+      "JavaScript",
+      "SpringBoot",
+      "Java",
+      "Python",
+      "HTML5",
+      "CSS3",
+      "Bootstrap",
+      "MySQL",
+      "SQLite",
+    ]
+  },
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://w7.pngwing.com/pngs/950/362/png-transparent-andhra-university-college-of-engineering-andhra-university-college-of-pharmaceutical-sciences-andhra-university-college-of-science-and-technology-jawaharlal-nehru-technological-university-thumbnail.png",
+    school: "Andhra University",
+    date: "Jun 2017 - Dec 2020",
+    grade: "6.91 CGPA",
+    desc: "I completed my graduation in Rajah RSRK Ranga Rao College, Bobbili. I have completed 6 semesters and have a CGPA of 6.91.",
+    degree: "Bachelor of Science",
+  },
+  {
+    id: 1,
+    img: "https://upload.wikimedia.org/wikipedia/en/b/b1/Boardofintermediateeducationlogo.png",
+    school: "Vidwan Junior College, Bobbili",
+    date: "Jun 2015 - Mar 2017",
+    grade: "886/1000 Marks",
+    desc: "I completed my class 12 high school education at Vidwan Juniour College, Bobbili, where I studied Mathematics, Physics and Chemistry.",
+    degree: "BIE (Board of Intermediate Education) - Andhra Pradesh",
+  },
+  {
+    id: 2,
+    img: "https://seeklogo.com/images/A/ap-govt-logo-1AAFDD42D1-seeklogo.com.png",
+    school: "Sri Chaitanya Vidya Niketan School, Bobbili",
+    date: "Jun 2014 - Mar 2015",
+    grade: "8.3 CGPA",
+    desc: "I completed my class 10 education at Sri Chaitanya Vidya Niketan School, Bobbili, where I studied Mathematics, Science and Social.",
+    degree: "SSC (School Secondary Certification) - Andhra Pradesh",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Jobby  App",
+    date: "Jan 2025 - Feb 2025",
+    description:
+      "Developed a job search platform where users can log in and view, search, and filter job listings based on job title and location.",
+    image:
+      "https://res.cloudinary.com/dl1subjhb/image/upload/v1744121272/Screenshot_2025-04-08_193530_smfx98.png",
+    tags: ["React.js", "JS", "CSS", "Rest API", "JWT Token", "Authorization", "Authentication"],
+    category: "Online Platform",
+    github: "https://github.com/sai-kumar-visai/Jobby-App.git",
+    webapp: "https://vskjobbyapk.ccbp.tech",
+  },
+  {
+    id: 1,
+    title: "NXT Trendz",
+    date: "Jan 2025 - Feb 2025",
+    description:
+      "Developed an e-commerce platform resembling Amazon and Flipkart using ReactJS for the front-end and Spring Boot for the back-end.",
+    image:
+      "https://res.cloudinary.com/dl1subjhb/image/upload/v1744121362/Screenshot_2025-04-08_193609_vvhcrl.png",
+    tags: ["React.js", "JS", "CSS", "Rest API", "JWT Token", "Authorization", "Authentication"],
+    category: "Online Platform",
+    github: "https://github.com/sai-kumar-visai/Nxt-Cart-App.git",
+    webapp: "https://vskcart.ccbp.tech",
+  },{
+    id: 2,
+    title: "Nxt Watch",
+    date: "Feb 2025 - OcFebt 2025",
+    description:
+      "Developed an OTT platform resembling YouTube and Nextflix using ReactJS for the front-end and Spring Boot for the back-end.",
+    image:
+      "https://res.cloudinary.com/dl1subjhb/image/upload/v1744122485/Screenshot_2025-04-08_195750_t7c9dw.png",
+    tags: ["React.js", "JS", "CSS", "Routing","Rest API", "JWT Token", "Authorization", "Authentication"],
+    category: "Online Platform",
+    github: "https://github.com/sai-kumar-visai/Nxt-watch-app.git",
+    webapp: "https://vskplayer.ccbp.tech/",
+  },{
+    id: 3,
+    title: "Emoji Game",
+    date: "Sep 2024 - Sep 2024",
+    description:
+      "Build a Responsive Emoji Game in intial stage of learning",
+    image:
+      "https://res.cloudinary.com/dl1subjhb/image/upload/v1744122778/Screenshot_2025-04-08_200221_qyzuhr.png",
+    tags: ["React.js", "JS", "CSS","Routing", "Bootstrap"],
+    category: "Responsive Mini Game",
+    github: "https://github.com/sai-kumar-visai/Emoji-Game.git",
+    webapp: "https://vskemoji.ccbp.tech/",
+  },{
+    id: 4,
+    title: "App Store",
+    date: "Nov 2024 - Dec 2024",
+    description: "Build a Dynamic App Store with few mobile apps",
+    image:
+      "https://res.cloudinary.com/dl1subjhb/image/upload/v1744125570/Screenshot_2025-04-08_200923_cv99gn.png",
+    tags: ["React.js", "JS", "CSS", "Bootstrap"],
+    category: "Dynamic Application",
+    github: "https://github.com/sai-kumar-visai/App-Store.git",
+    webapp: "https://vsplaystoreapps.ccbp.tech/",
+  },{
+    id: 5,
+    title: "Matching Game",
+    date: "Jun 2024 - Jul 2024",
+    description:
+      "Build a Responsive Matching Game in intial stage of learning",
+    image:
+      "https://res.cloudinary.com/dl1subjhb/image/upload/v1744125801/Screenshot_2025-04-08_205241_oevspk.png",
+    tags: ["React.js", "JS", "CSS", "Routing"],
+    category: "Responsive Mini Game",
+    github: "https://github.com/sai-kumar-visai/Matching-Game-.git",
+    webapp: "https://vskmatch.ccbp.tech/",
+  },{
+    id: 6,
+    title: "Music Playlist",
+    date: "Sep 2024 - Oct 2024",
+    description:
+      "Developed an online music platform resembling Spotiy and Gaana using ReactJS for the front-end and Spring Boot for the back-end.",
+    image:
+      "https://res.cloudinary.com/dl1subjhb/image/upload/v1744125559/Screenshot_2025-04-08_202746_dvmmul.png",
+    tags: ["React.js", "JS", "CSS", "Rest API", "JWT Token"],
+    category: "Online Platform",
+    github: "https://github.com/sai-kumar-visai/Music-Playlist.git",
+    webapp: "https://vskmusic.ccbp.tech/",
+  },{
+    id: 7,
+    title: "Appointment App",
+    date: "Nov 2024 - Dec 2024",
+    description:
+      "Build a Dynamic Appointment app for advanced booking",
+    image:
+      "https://res.cloudinary.com/dl1subjhb/image/upload/v1744125537/Screenshot_2025-04-08_204605_ka0jq1.png",
+    tags: ["React.js", "JS", "CSS"],
+    category: "Dynamic Application",
+    github: "https://github.com/sai-kumar-visai/Appointment-App.git",
+    webapp: "https://skappapp.ccbp.tech/",
+  },{
+    id: 8,
+    title: "Rock-Paper-Scissors",
+    date: "Jul 2024 - Jul 2024",
+    description:
+      "Build a Responsive Funny Game for childrens",
+    image:
+      "https://res.cloudinary.com/dl1subjhb/image/upload/v1744126664/Screenshot_2025-04-08_210641_a56v83.png",
+    tags: ["React.js", "JS", "CSS", "Routing"],
+    category: "Responsive Mini Game",
+    github: "https://github.com/sai-kumar-visai/Rock-Paper-Scissors-Game.git",
+    webapp: "https://vskrps.ccbp.tech/",
+  },{
+    id: 9,
+    title: "Cash Withdraw",
+    date: "Aug 2024 - Sep 2024",
+    description:
+      "Build a Dynamic web application for Cash Withdrawl",
+    image:
+      "https://res.cloudinary.com/dl1subjhb/image/upload/v1744125542/Screenshot_2025-04-08_203530_e28akc.png",
+    tags: ["React.js", "JS", "CSS","Routing"],
+    category: "Dynamic Application",
+    github: "https://github.com/sai-kumar-visai/Cash-Withdraw.git",
+    webapp: "https://vskcash.ccbp.tech/",
+  }
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2024, text: "Shared my projects with the world" },
+  { year: 2025, text: "Started my own platform" },
+];
